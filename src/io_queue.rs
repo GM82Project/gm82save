31f24b7637cd80b2e8b55d40a9d02c112ee0b69c
Floogle <18466542+skyfloogle@users.noreply.github.com>
2021-04-24 14:53:31 +0200

    toggleable write splitting and better allocation

--- src/io_queue.rs
+++ src/io_queue.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs::File,
     io::{Error, ErrorKind, Write},
     mem::take,
     path::{Path, PathBuf},
@@ -6,8 +7,12 @@ use std::{
     thread::{spawn, JoinHandle},
 };
 
+const DEFAULT_CAPACITY: usize = 8192;
+const SPLIT_WRITES: bool = false;
+
 enum Command {
-    File(PathBuf, Vec<u8>),
+    Open(PathBuf, Vec<u8>),
+    Append(Vec<u8>),
     Finish,
 }
 
@@ -21,11 +26,45 @@ impl IOThread {
         Self { queue, error }
     }
 
+    fn open(path: PathBuf, buf: Vec<u8>) -> std::io::Result<File> {
+        let mut f = File::create(path)?;
+        f.write_all(&buf)?;
+        Ok(f)
+    }
+
     fn run_thread(self) -> Result<(), ()> {
+        // i'd love to not have this epic code duplication but not totally sure how
+        let mut file = match self.queue.recv() {
+            Ok(Command::Open(path, buffer)) => match Self::open(path, buffer) {
+                Ok(f) => f,
+                Err(e) => {
+                    let _ = self.error.send(e);
+                    return Err(())
+                },
+            },
+            Ok(Command::Finish) => return Ok(()),
+            Ok(Command::Append(_)) => {
+                let _ = self.error.send(Error::new(ErrorKind::Other, "no file open"));
+                return Err(())
+            },
+            Err(_) => {
+                let _ = self.error.send(ErrorKind::BrokenPipe.into());
+                return Err(())
+            },
+        };
         loop {
             match self.queue.recv() {
-                Ok(Command::File(path, buffer)) => {
-                    if let Err(e) = std::fs::write(path, buffer) {
+                Ok(Command::Open(path, buffer)) => {
+                    file = match Self::open(path, buffer) {
+                        Ok(f) => f,
+                        Err(e) => {
+                            let _ = self.error.send(e);
+                            return Err(())
+                        },
+                    };
+                },
+                Ok(Command::Append(buf)) => {
+                    if let Err(e) = file.write_all(&buf) {
                         let _ = self.error.send(e);
                         return Err(())
                     }
@@ -57,11 +96,16 @@ impl IOController {
         if let Ok(err) = self.error.try_recv() {
             return Err(err)
         }
-        Ok(IORequest { controller: self, path: path.to_path_buf(), buffer: Vec::with_capacity(8192) })
+        Ok(IORequest {
+            controller: self,
+            path: path.to_path_buf(),
+            buffer: Vec::with_capacity(DEFAULT_CAPACITY),
+            sent_first: false,
+        })
     }
 
     pub fn write_file(&self, path: &Path, content: Vec<u8>) -> std::io::Result<()> {
-        let _ = self.queue.send(Command::File(path.to_path_buf(), content));
+        let _ = self.queue.send(Command::Open(path.to_path_buf(), content));
         if let Ok(err) = self.error.try_recv() {
             return Err(err)
         }
@@ -82,11 +126,39 @@ pub struct IORequest<'a> {
     controller: &'a IOController,
     path: PathBuf,
     buffer: Vec<u8>,
+    sent_first: bool,
+}
+
+impl<'a> IORequest<'a> {
+    fn send_buf(&mut self) {
+        let _ = self.controller.queue.send(if self.sent_first {
+            Command::Append(take(&mut self.buffer))
+        } else {
+            self.sent_first = true;
+            // this will invalidate self.path but whatever
+            Command::Open(take(&mut self.path), take(&mut self.buffer))
+        });
+    }
 }
 
 impl<'a> Write for IORequest<'a> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.buffer.extend_from_slice(buf);
+        if SPLIT_WRITES {
+            let leftover_cap = self.buffer.capacity() - self.buffer.len();
+            if buf.len() > leftover_cap {
+                self.buffer.extend_from_slice(&buf[..leftover_cap]);
+                self.send_buf();
+                self.buffer.reserve(DEFAULT_CAPACITY);
+                self.buffer.extend_from_slice(&buf[leftover_cap..]);
+            } else {
+                self.buffer.extend_from_slice(buf);
+            }
+        } else {
+            if self.buffer.len() + buf.len() > self.buffer.capacity() {
+                self.buffer.reserve(buf.len() - self.buffer.len() + DEFAULT_CAPACITY);
+            }
+            self.buffer.extend_from_slice(buf);
+        }
         if let Ok(err) = self.controller.error.try_recv() {
             return Err(err)
         }
@@ -100,6 +172,6 @@ impl<'a> Write for IORequest<'a> {
 
 impl<'a> Drop for IORequest<'a> {
     fn drop(&mut self) {
-        let _ = self.controller.queue.send(Command::File(take(&mut self.path), take(&mut self.buffer)));
+        self.send_buf();
     }
 }
