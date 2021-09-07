5204227820643359234f1ea3dbea0141bc067fa1
Floogle <18466542+skyfloogle@users.noreply.github.com>
2021-09-07 22:05:28 +0200

    use new delphi list wrapper for ide pointers

--- src/ide.rs
+++ src/ide.rs
@@ -1,120 +1,155 @@
 #![allow(dead_code)]
 use crate::{
     asset::*,
-    delphi::{self, TTreeNode, TTreeView, UStr},
+    delphi::{TTreeNode, TTreeView, UStr},
+    list::DelphiList,
 };
 use std::slice;
 
 type IntPtr = *mut usize;
-type AssetList<T> = *mut *mut Option<&'static T>;
-type GaplessList<T> = *mut *mut &'static T;
-type UStrListPtr = *mut *mut UStr;
-type Forms = *mut *mut Form;
-type Timestamps = *mut *mut f64;
-type ThumbIDs = *mut *mut i32;
-type TypeInfoPtr = *const u8;
+type GaplessList<T, const P: usize> = *mut DelphiList<&'static T, P>;
+type TypeInfoPtr = usize;
+
+#[repr(C)]
+struct AssetList<T: 'static, const P1: usize, const P2: usize, const P3: usize, const P4: usize> {
+    pub assets: DelphiList<Option<&'static T>, P1>,
+    pub forms: DelphiList<Form, P2>,
+    pub names: DelphiList<UStr, P3>,
+    pub timestamps: DelphiList<f64, P4>,
+    pub count: usize,
+}
+
+#[repr(C)]
+struct GraphicAssetList<T: 'static, const P1: usize, const P2: usize, const P3: usize, const P4: usize, const P5: usize>
+{
+    pub assets: DelphiList<Option<&'static T>, P1>,
+    pub forms: DelphiList<Form, P2>,
+    pub names: DelphiList<UStr, P3>,
+    pub timestamps: DelphiList<f64, P4>,
+    pub thumbs: DelphiList<i32, P5>,
+    pub count: usize,
+}
 
 const TRIGGER_COUNT: IntPtr = 0x77f3f8 as _;
-const TRIGGERS: AssetList<Trigger> = 0x77f3f4 as _;
+const TRIGGERS: *mut DelphiList<Option<&'static Trigger>, 0x6bc93c> = 0x77f3f4 as _;
 pub const TRIGGERS_UPDATED: *const bool = 0x790058 as _;
 
 const CONSTANT_COUNT: IntPtr = 0x77f3c4 as _;
-const CONSTANT_NAMES: UStrListPtr = 0x78c14c as _;
-const CONSTANT_VALUES: UStrListPtr = 0x78c150 as _;
+const CONSTANT_NAMES: *mut DelphiList<UStr, 0x696594> = 0x78c14c as _;
+const CONSTANT_VALUES: *mut DelphiList<UStr, 0x6965c0> = 0x78c150 as _;
 const CONSTANT_NAME_TYPE: TypeInfoPtr = 0x696594 as _;
 const CONSTANT_VALUE_TYPE: TypeInfoPtr = 0x6965c0 as _;
 pub const CONSTANTS_UPDATED: *mut bool = 0x78c154 as _;
 
-const SOUNDS: AssetList<Sound> = 0x77f2b8 as _;
-const SOUND_FORMS: Forms = 0x77f2bc as _;
-const SOUND_NAMES: UStrListPtr = 0x77f2c0 as _;
-const SOUND_TIMESTAMPS: Timestamps = 0x77f2c4 as _;
-const SOUND_COUNT: IntPtr = 0x77f2c8 as _;
+const SOUNDS: *mut AssetList<
+    Sound,
+    SOUND_TYPEINFO,
+    { SOUND_TYPEINFO + SOUND_TYPESIZE },
+    { SOUND_TYPEINFO + SOUND_TYPESIZE * 2 },
+    { SOUND_TYPEINFO + SOUND_TYPESIZE * 3 },
+> = 0x77f2b8 as _;
 const SOUND_TYPEINFO: TypeInfoPtr = 0x651ce0 as _;
 const SOUND_TYPESIZE: usize = 0x2c;
 pub const SOUNDS_UPDATED: *mut bool = 0x78a1b0 as _;
 
-const SPRITES: AssetList<Sprite> = 0x77f4c4 as _;
-const SPRITE_NAMES: UStrListPtr = 0x77f4cc as _;
-const SPRITE_TIMESTAMPS: Timestamps = 0x77f4d0 as _;
-const SPRITE_THUMBS: ThumbIDs = 0x77f4d4 as _;
-const SPRITE_COUNT: IntPtr = 0x77f4d8 as _;
+const SPRITES: *mut GraphicAssetList<
+    Sprite,
+    SPRITE_TYPEINFO,
+    { SPRITE_TYPEINFO + SPRITE_TYPESIZE },
+    { SPRITE_TYPEINFO + SPRITE_TYPESIZE * 2 },
+    { SPRITE_TYPEINFO + SPRITE_TYPESIZE * 3 },
+    { SPRITE_TYPEINFO + SPRITE_TYPESIZE * 4 },
+> = 0x77f4c4 as _;
 const SPRITE_TYPEINFO: TypeInfoPtr = 0x6f522c as _;
 const SPRITE_TYPESIZE: usize = 0x2c;
 pub const SPRITES_UPDATED: *mut bool = 0x790170 as _;
 
-const BACKGROUNDS: AssetList<Background> = 0x77f1ac as _;
-const BACKGROUND_FORMS: Forms = 0x77f1b0 as _;
-const BACKGROUND_NAMES: UStrListPtr = 0x77f1b4 as _;
-const BACKGROUND_TIMESTAMPS: Timestamps = 0x77f1b8 as _;
-const BACKGROUND_THUMBS: ThumbIDs = 0x77f1bc as _;
-const BACKGROUND_COUNT: IntPtr = 0x77f1c0 as _;
+const BACKGROUNDS: *mut GraphicAssetList<
+    Background,
+    BACKGROUND_TYPEINFO,
+    { BACKGROUND_TYPEINFO + BACKGROUND_TYPESIZE },
+    { BACKGROUND_TYPEINFO + BACKGROUND_TYPESIZE * 2 },
+    { BACKGROUND_TYPEINFO + BACKGROUND_TYPESIZE * 3 },
+    { BACKGROUND_TYPEINFO + BACKGROUND_TYPESIZE * 4 },
+> = 0x77f1ac as _;
 const BACKGROUND_TYPEINFO: TypeInfoPtr = 0x64d734 as _;
 const BACKGROUND_TYPESIZE: usize = 0x30;
 pub const BACKGROUNDS_UPDATED: *mut bool = 0x78a168 as _;
 
-const PATHS: AssetList<Path> = 0x77f608 as _;
-const PATH_FORMS: Forms = 0x77f60c as _;
-const PATH_NAMES: UStrListPtr = 0x77f610 as _;
-const PATH_TIMESTAMPS: Timestamps = 0x77f614 as _;
-const PATH_COUNT: IntPtr = 0x77f618 as _;
+const PATHS: *mut AssetList<
+    Path,
+    PATH_TYPEINFO,
+    { PATH_TYPEINFO + PATH_TYPESIZE },
+    { PATH_TYPEINFO + PATH_TYPESIZE * 2 },
+    { PATH_TYPEINFO + PATH_TYPESIZE * 3 },
+> = 0x77f608 as _;
 const PATH_TYPEINFO: TypeInfoPtr = 0x72207c as _;
 const PATH_TYPESIZE: usize = 0x28;
 pub const PATHS_UPDATED: *mut bool = 0x7a4658 as _;
 
-const SCRIPTS: AssetList<Script> = 0x77f2cc as _;
-const SCRIPT_FORMS: Forms = 0x77f2d0 as _;
-const SCRIPT_NAMES: UStrListPtr = 0x77f2d4 as _;
-const SCRIPT_TIMESTAMPS: Timestamps = 0x77f2d8 as _;
-const SCRIPT_COUNT: IntPtr = 0x77f2dc as _;
+const SCRIPTS: *mut AssetList<
+    Script,
+    SCRIPT_TYPEINFO,
+    { SCRIPT_TYPEINFO + SCRIPT_TYPESIZE },
+    { SCRIPT_TYPEINFO + SCRIPT_TYPESIZE * 2 },
+    { SCRIPT_TYPEINFO + SCRIPT_TYPESIZE * 3 },
+> = 0x77f2cc as _;
 const SCRIPT_TYPEINFO: TypeInfoPtr = 0x6550a8 as _;
 const SCRIPT_TYPESIZE: usize = 0x2c;
 pub const SCRIPTS_UPDATED: *mut bool = 0x78a1b8 as _;
 
-const FONTS: AssetList<Font> = 0x77f4fc as _;
-const FONT_FORMS: Forms = 0x77f500 as _;
-const FONT_NAMES: UStrListPtr = 0x77f504 as _;
-const FONT_TIMESTAMPS: Timestamps = 0x77f508 as _;
-const FONT_COUNT: IntPtr = 0x77f50c as _;
+const FONTS: *mut AssetList<
+    Font,
+    FONT_TYPEINFO,
+    { FONT_TYPEINFO + FONT_TYPESIZE },
+    { FONT_TYPEINFO + FONT_TYPESIZE * 2 },
+    { FONT_TYPEINFO + FONT_TYPESIZE * 3 },
+> = 0x77f4fc as _;
 const FONT_TYPEINFO: TypeInfoPtr = 0x6fc680 as _;
 const FONT_TYPESIZE: usize = 0x28;
 pub const FONTS_UPDATED: *mut bool = 0x790190 as _;
 
-const TIMELINES: AssetList<Timeline> = 0x77f4e4 as _;
-const TIMELINE_FORMS: Forms = 0x77f4e8 as _;
-const TIMELINE_NAMES: UStrListPtr = 0x77f4ec as _;
-const TIMELINE_TIMESTAMPS: Timestamps = 0x77f4f0 as _;
-const TIMELINE_COUNT: IntPtr = 0x77f4f4 as _;
+const TIMELINES: *mut AssetList<
+    Timeline,
+    TIMELINE_TYPEINFO,
+    { TIMELINE_TYPEINFO + TIMELINE_TYPESIZE },
+    { TIMELINE_TYPEINFO + TIMELINE_TYPESIZE * 2 },
+    { TIMELINE_TYPEINFO + TIMELINE_TYPESIZE * 3 },
+> = 0x77f4e4 as _;
 const TIMELINE_TYPEINFO: TypeInfoPtr = 0x6fa020 as _;
 const TIMELINE_TYPESIZE: usize = 0x2c;
 pub const TIMELINES_UPDATED: *mut bool = 0x790188 as _;
 
-const OBJECTS: AssetList<Object> = 0x77f0d0 as _;
-const OBJECT_FORMS: Forms = 0x77f0d4 as _;
-const OBJECT_NAMES: UStrListPtr = 0x77f0d8 as _;
-const OBJECT_TIMESTAMPS: Timestamps = 0x77f0dc as _;
-const OBJECT_COUNT: IntPtr = 0x77f0e0 as _;
+const OBJECTS: *mut AssetList<
+    Object,
+    OBJECT_TYPEINFO,
+    { OBJECT_TYPEINFO + OBJECT_TYPESIZE },
+    { OBJECT_TYPEINFO + OBJECT_TYPESIZE * 2 },
+    { OBJECT_TYPEINFO + OBJECT_TYPESIZE * 3 },
+> = 0x77f0d0 as _;
 const OBJECT_TYPEINFO: TypeInfoPtr = 0x62c4a8 as _;
 const OBJECT_TYPESIZE: usize = 0x2c;
 pub const OBJECTS_UPDATED: *mut bool = 0x78a0d4 as _;
 
-const ROOMS: AssetList<Room> = 0x77f3a8 as _;
-const ROOM_FORMS: Forms = 0x77f3ac as _;
-const ROOM_NAMES: UStrListPtr = 0x77f3b0 as _;
-const ROOM_TIMESTAMPS: Timestamps = 0x77f3b4 as _;
-const ROOM_COUNT: IntPtr = 0x77f3b8 as _;
+const ROOMS: *mut AssetList<
+    Room,
+    ROOM_TYPEINFO,
+    { ROOM_TYPEINFO + ROOM_TYPESIZE },
+    { ROOM_TYPEINFO + ROOM_TYPESIZE * 2 },
+    { ROOM_TYPEINFO + ROOM_TYPESIZE * 3 },
+> = 0x77f3a8 as _;
 const ROOM_TYPEINFO: TypeInfoPtr = 0x6928f8 as _;
 const ROOM_TYPESIZE: usize = 0x28;
 pub const ROOMS_UPDATED: *mut bool = 0x78a1f8 as _;
 
-const INCLUDED_FILES: GaplessList<IncludedFile> = 0x77f420 as _;
-const INCLUDED_FILE_TIMESTAMPS: Timestamps = 0x77f424 as _;
+const INCLUDED_FILES: GaplessList<IncludedFile, 0x6cc47c> = 0x77f420 as _;
+const INCLUDED_FILE_TIMESTAMPS: *mut DelphiList<f64, 0x6cc4a8> = 0x77f424 as _;
 const INCLUDED_FILE_COUNT: IntPtr = 0x77f428 as _;
 pub const INCLUDED_FILES_UPDATED: *mut bool = 0x7900a0 as _;
 
-const EXTENSIONS: GaplessList<Extension> = 0x77f5d4 as _;
+const EXTENSIONS: GaplessList<Extension, 0x712788> = 0x77f5d4 as _;
 const EXTENSION_COUNT: IntPtr = 0x77f5d8 as _;
-const EXTENSIONS_LOADED: *mut *mut bool = 0x790a14 as _;
+const EXTENSIONS_LOADED: *mut DelphiList<bool, 0x7127b8> = 0x790a14 as _;
 pub const EXTENSIONS_UPDATED: *mut bool = 0x790a0c as _;
 
 pub const GAME_ID: IntPtr = 0x7907f4 as IntPtr;
@@ -223,49 +258,52 @@ pub fn initialize_project() {
 macro_rules! read_array {
     ($n:ident, $t:ty, $p:expr, $c:expr) => {
         pub fn $n<'a>() -> &'a [$t] {
-            unsafe { slice::from_raw_parts(($p).read(), $c.read()) }
+            unsafe { $p.get_unchecked(..$c) }
         }
 
         paste::paste! {
             pub fn [<$n _mut>]<'a>() -> &'a mut [$t] {
-                unsafe { slice::from_raw_parts_mut(($p).read(), $c.read()) }
+                unsafe { $p.get_unchecked_mut(..$c) }
             }
         }
     };
 }
 
 macro_rules! get_assets {
-    ($lo:ident, $t:ty, $assets_p:expr, $count_p:expr, $type_p:expr, $type_size:expr) => {
+    ($lo:ident, $t:ty, $assets_p:expr) => {
         paste::paste! {
-            read_array!([<get_ $lo s>], Option<&'static $t>, $assets_p, $count_p);
-            read_array!([<get_ $lo _forms>], Form, $assets_p.add(1) as Forms, $count_p);
-            read_array!([<get_ $lo _names>], UStr, $assets_p.add(2) as UStrListPtr, $count_p);
-            read_array!([<get_ $lo _timestamps>], f64, $assets_p.add(3) as Timestamps, $count_p);
+            read_array!([<get_ $lo s>], Option<&'static $t>, (*$assets_p).assets, (*$assets_p).count);
+            read_array!([<get_ $lo _forms>], Form, (*$assets_p).forms, (*$assets_p).count);
+            read_array!([<get_ $lo _names>], UStr, (*$assets_p).names, (*$assets_p).count);
+            read_array!([<get_ $lo _timestamps>], f64, (*$assets_p).timestamps, (*$assets_p).count);
             pub fn [<alloc_ $lo s>](count: usize) {
                 unsafe {
-                    $count_p.write(count);
-                    for i in 0..4 {
-                        delphi::DynArraySetLength($assets_p.add(i), $type_p.add($type_size*i), 1, count);
-                    }
+                    (*$assets_p).count = count;
+                    (*$assets_p).assets.alloc(count);
+                    (*$assets_p).forms.alloc(count);
+                    (*$assets_p).names.alloc(count);
+                    (*$assets_p).timestamps.alloc(count);
                 }
             }
         }
     };
 }
 macro_rules! get_graphics {
-    ($lo:ident, $t:ty, $assets_p:expr, $count_p:expr, $type_p:expr, $type_size:expr) => {
+    ($lo:ident, $t:ty, $assets_p:expr) => {
         paste::paste! {
-            read_array!([<get_ $lo s>], Option<&'static $t>, $assets_p, $count_p);
-            read_array!([<get_ $lo _forms>], Form, $assets_p.add(1) as Forms, $count_p);
-            read_array!([<get_ $lo _names>], UStr, $assets_p.add(2) as UStrListPtr, $count_p);
-            read_array!([<get_ $lo _timestamps>], f64, $assets_p.add(3) as Timestamps, $count_p);
-            read_array!([<get_ $lo _thumbs>], i32, $assets_p.add(4) as ThumbIDs, $count_p);
+            read_array!([<get_ $lo s>], Option<&'static $t>, (*$assets_p).assets, (*$assets_p).count);
+            read_array!([<get_ $lo _forms>], Form, (*$assets_p).forms, (*$assets_p).count);
+            read_array!([<get_ $lo _names>], UStr, (*$assets_p).names, (*$assets_p).count);
+            read_array!([<get_ $lo _timestamps>], f64, (*$assets_p).timestamps, (*$assets_p).count);
+            read_array!([<get_ $lo _thumbs>], i32, (*$assets_p).thumbs, (*$assets_p).count);
             pub fn [<alloc_ $lo s>](count: usize) {
                 unsafe {
-                    $count_p.write(count);
-                    for i in 0..5 {
-                        delphi::DynArraySetLength($assets_p.add(i), $type_p.add($type_size*i), 1, count);
-                    }
+                    (*$assets_p).count = count;
+                    (*$assets_p).assets.alloc(count);
+                    (*$assets_p).forms.alloc(count);
+                    (*$assets_p).names.alloc(count);
+                    (*$assets_p).timestamps.alloc(count);
+                    (*$assets_p).thumbs.alloc(count);
                     [<get_ $lo _thumbs_mut>]().fill(-1);
                 }
             }
@@ -273,23 +311,23 @@ macro_rules! get_graphics {
     };
 }
 
-get_assets!(sound, Sound, SOUNDS, SOUND_COUNT, SOUND_TYPEINFO, SOUND_TYPESIZE);
-get_graphics!(sprite, Sprite, SPRITES, SPRITE_COUNT, SPRITE_TYPEINFO, SPRITE_TYPESIZE);
-get_graphics!(background, Background, BACKGROUNDS, BACKGROUND_COUNT, BACKGROUND_TYPEINFO, BACKGROUND_TYPESIZE);
-get_assets!(path, Path, PATHS, PATH_COUNT, PATH_TYPEINFO, PATH_TYPESIZE);
-get_assets!(script, Script, SCRIPTS, SCRIPT_COUNT, SCRIPT_TYPEINFO, SCRIPT_TYPESIZE);
-get_assets!(font, Font, FONTS, FONT_COUNT, FONT_TYPEINFO, FONT_TYPESIZE);
-get_assets!(timeline, Timeline, TIMELINES, TIMELINE_COUNT, TIMELINE_TYPEINFO, TIMELINE_TYPESIZE);
-get_assets!(object, Object, OBJECTS, OBJECT_COUNT, OBJECT_TYPEINFO, OBJECT_TYPESIZE);
-get_assets!(room, Room, ROOMS, ROOM_COUNT, ROOM_TYPEINFO, ROOM_TYPESIZE);
+get_assets!(sound, Sound, SOUNDS);
+get_graphics!(sprite, Sprite, SPRITES);
+get_graphics!(background, Background, BACKGROUNDS);
+get_assets!(path, Path, PATHS);
+get_assets!(script, Script, SCRIPTS);
+get_assets!(font, Font, FONTS);
+get_assets!(timeline, Timeline, TIMELINES);
+get_assets!(object, Object, OBJECTS);
+get_assets!(room, Room, ROOMS);
 
-read_array!(get_constant_names, UStr, CONSTANT_NAMES, CONSTANT_COUNT);
-read_array!(get_constants, UStr, CONSTANT_VALUES, CONSTANT_COUNT);
-read_array!(get_triggers, Option<&'static Trigger>, TRIGGERS, TRIGGER_COUNT);
+read_array!(get_constant_names, UStr, *CONSTANT_NAMES, *CONSTANT_COUNT);
+read_array!(get_constants, UStr, *CONSTANT_VALUES, *CONSTANT_COUNT);
+read_array!(get_triggers, Option<&'static Trigger>, *TRIGGERS, *TRIGGER_COUNT);
 
-read_array!(get_included_files, &'static IncludedFile, INCLUDED_FILES, INCLUDED_FILE_COUNT);
-read_array!(get_extensions, &'static Extension, EXTENSIONS, EXTENSION_COUNT);
-read_array!(get_extensions_loaded, bool, EXTENSIONS_LOADED, EXTENSION_COUNT);
+read_array!(get_included_files, &'static IncludedFile, *INCLUDED_FILES, *INCLUDED_FILE_COUNT);
+read_array!(get_extensions, &'static Extension, *EXTENSIONS, *EXTENSION_COUNT);
+read_array!(get_extensions_loaded, bool, *EXTENSIONS_LOADED, *EXTENSION_COUNT);
 
 pub fn get_action_libraries<'a>() -> &'a [&'a ActionLibrary] {
     unsafe { slice::from_raw_parts(ACTION_LIBRARIES, ACTION_LIBRARY_COUNT.read()) }
@@ -298,22 +336,22 @@ pub fn get_action_libraries<'a>() -> &'a [&'a ActionLibrary] {
 pub fn alloc_constants(count: usize) {
     unsafe {
         CONSTANT_COUNT.write(count);
-        delphi::DynArraySetLength(CONSTANT_NAMES, CONSTANT_NAME_TYPE, 1, count);
-        delphi::DynArraySetLength(CONSTANT_VALUES, CONSTANT_VALUE_TYPE, 1, count);
+        (*CONSTANT_NAMES).alloc(count);
+        (*CONSTANT_VALUES).alloc(count);
     }
 }
 
 pub fn alloc_triggers(count: usize) {
     unsafe {
         TRIGGER_COUNT.write(count);
-        delphi::DynArraySetLength(TRIGGERS, 0x6bc93c as TypeInfoPtr, 1, count);
+        (*TRIGGERS).alloc(count);
     }
 }
 
 pub fn alloc_included_files(count: usize) {
     unsafe {
         INCLUDED_FILE_COUNT.write(count);
-        delphi::DynArraySetLength(INCLUDED_FILES, 0x6cc47c as _, 1, count);
-        delphi::DynArraySetLength(INCLUDED_FILE_TIMESTAMPS, 0x6cc4a8 as _, 1, count);
+        (*INCLUDED_FILES).alloc(count);
+        (*INCLUDED_FILE_TIMESTAMPS).alloc(count);
     }
 }
