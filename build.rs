e90b4dbd8b01f6ea0325e3915fff97ed612a2074
Floogle <18466542+skyfloogle@users.noreply.github.com>
2022-12-18 10:30:09 +0100

    fmt

--- build.rs
+++ build.rs
@@ -3,6 +3,13 @@ use time::OffsetDateTime;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let date = OffsetDateTime::now_utc();
     println!("cargo:rustc-env=ABOUT_BUILD_DATE={} {}, {}", date.month(), date.day(), date.year());
-    println!("cargo:rustc-env=ERROR_BUILD_DATE={}{:02}{:02}T{:02}{:02}", date.year(), u8::from(date.month()), date.day(), date.hour(), date.minute());
+    println!(
+        "cargo:rustc-env=ERROR_BUILD_DATE={}{:02}{:02}T{:02}{:02}",
+        date.year(),
+        u8::from(date.month()),
+        date.day(),
+        date.hour(),
+        date.minute()
+    );
     Ok(())
 }
